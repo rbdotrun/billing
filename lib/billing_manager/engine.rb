0c689d027d80f4c3b08d3f994a498550342bbb8b
Ben <ben@dee.mx>
2024-03-18 18:09:28 +0100

    Update engine.rb

--- lib/billing_manager/engine.rb
+++ lib/billing_manager/engine.rb
@@ -7,5 +7,13 @@ module BillingManager
       g.fixture_replacement(:factory_bot)
       g.factory_bot(dir: "spec/factories")
     end
+
+    initializer(:append_migrations) do |app|
+      unless app.root.to_s.match root.to_s
+        config.paths["db/migrate"].expanded.each do |expanded_path|
+          app.config.paths["db/migrate"] << expanded_path
+        end
+      end
+    end
   end
 end
